50673b236bc426ef2542aabb0f943f368b1b24de
Adolfo Aldana <adlf.aldana@gmail.com>
2023-08-10 21:03:35 -0400

    #CHANGE: add colors random background

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
 
 function App() {
+  const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "black", "white"];
+  const [color, setColor] = useState("yellow");
 
   const changeColor = () => {
-    document.body.style.backgroundColor = "red";
+    document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
   }
-  
+
+  useEffect(() => {
+    changeColor();
+  }, [color]);
+
   return (
     <div>
       <div>
